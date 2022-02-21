38e4b481339830c3bda79ddd8e910cb4bc4480ed
barrosAlessandro <alessandro.eletronica@gmail.com>
2022-02-21 16:20:39 -0400

    Tela create session finalizada

--- flutter_avancado/lib/screens/session_create/create_sessions.dart
+++ flutter_avancado/lib/screens/session_create/create_sessions.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_avancado/screens/session_create/components/colors_sessions.dart';
 import 'package:flutter_avancado/utils/constants.dart';
@@ -12,7 +11,6 @@ class CreateSessions extends StatefulWidget{
   State<StatefulWidget> createState() {
     return _CreateSessions();
   }
-
   
 }
 
@@ -23,7 +21,18 @@ class _CreateSessions extends State{
       appBar: AppBar(
         title: const Text('Create Session' , style: TextStyle(color: kPrimaryColor, fontSize: 25.0)),
         backgroundColor: kWhiteColor,
-        iconTheme: const IconThemeData(color: kPrimaryColor)
+        iconTheme: const IconThemeData(color: kPrimaryColor),
+        actions: 
+          <Widget>[
+            IconButton(
+              icon: const Icon(Icons.done, color: Colors.green),
+              tooltip: 'Notifications',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Função não implementada')));
+              },
+            )
+          ],
       ),
 
       body: const BodyCreateSessions(),
@@ -41,82 +50,105 @@ class BodyCreateSessions extends StatelessWidget{
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
 
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.only(top: 60),
-        width: screenSize.width - 100,
-        height: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const TextField(
-              style: TextStyle(fontSize: 18),
-              cursorColor: Colors.grey,        
-              decoration: InputDecoration(
-                floatingLabelBehavior: FloatingLabelBehavior.auto,
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: kPrimaryColor)
-                ),
-                labelText: 'Title',
-                floatingLabelStyle: TextStyle(color: kPrimaryColor)
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.only(top: 50.0),
-              child: TextField(
-              style: TextStyle(fontSize: 18),
-              maxLines: 5,
-                maxLength: 30,
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Center(
+        child: Container(
+          margin: const EdgeInsets.only(top: 60),
+          width: screenSize.width - 100,
+          // height: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const TextField(
+                style: TextStyle(fontSize: 18),
                 cursorColor: Colors.grey,        
                 decoration: InputDecoration(
-                  alignLabelWithHint: true,
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
                   border: OutlineInputBorder(),
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: kPrimaryColor)
                   ),
-                  labelText: 'Description',
-                  
+                  labelText: 'Title',
                   floatingLabelStyle: TextStyle(color: kPrimaryColor)
                 ),
               ),
-            ),
 
-            Container(
-              margin: EdgeInsets.only(top: 30.0),
-              child: Text('Select a color', style: TextStyle(color: Color.fromARGB(255, 136, 136, 136), fontWeight: FontWeight.bold, fontSize: 18)),
-            ),
-
-            GridView.builder(
-              
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,              
-              gridDelegate: 
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, crossAxisSpacing: 20, mainAxisSpacing: 10, childAspectRatio: 1),
-              padding: const EdgeInsets.only(top: 20),
-              itemCount: colorsList.length,
-              itemBuilder: (BuildContext context, int index) {
-                // final item = plantsList[plantsList.length - 1 - index];
-                return Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: colorsList[index].color
+              Container(
+                margin: EdgeInsets.only(top: 50.0),
+                child: TextField(
+                style: TextStyle(fontSize: 18),
+                maxLines: 5,
+                  maxLength: 30,
+                  cursorColor: Colors.grey,        
+                  decoration: InputDecoration(
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: kPrimaryColor)
+                    ),
+                    labelText: 'Description',
+                    
+                    floatingLabelStyle: TextStyle(color: kPrimaryColor)
                   ),
-                );
-              }
-            )
+                ),
+              ),
 
+              Container(
+                margin: EdgeInsets.only(top: 30.0),
+                child: Text('Select a color', style: TextStyle(color: Color.fromARGB(255, 136, 136, 136), fontWeight: FontWeight.bold, fontSize: 18)),
+              ),
 
-          ],
+              SetColor()
+            ],
+          ),
         ),
+
       ),
-
     );
-
   }
+}
 
+class SetColor extends StatefulWidget{
+  const SetColor({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() {
+    return _SetColor();
+  }
+}
 
+class _SetColor extends State<SetColor>{
+  int selectedColor = -1;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,              
+      gridDelegate: 
+        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4, crossAxisSpacing: 20, mainAxisSpacing: 10, childAspectRatio: 1),
+      padding: const EdgeInsets.only(top: 20),
+      itemCount: colorsList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap:(){
+            setState(() {
+              selectedColor = index;
+              
+            });
+          },
+          child: Container(
+            child: selectedColor == index ? Icon(Icons.check_circle_rounded, size: 32) : null,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: colorsList[index].color
+            ),
+          ),
+        );
+      }
+    );
+  }
 }
